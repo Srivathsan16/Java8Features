26fa0ca40e1e3f3f6a371897e32a3bcecc5fa9c0
Srivathsan <srivathsanrajagopal@gmail.com>
2018-08-19 18:31:38 +0530

    Sreams In Java 8

--- src/java8/features/streams/StreamsImplementation.java
+++ src/java8/features/streams/StreamsImplementation.java
@@ -1,6 +1,21 @@
 package java8.features.streams;
 
+import java.util.*;
+
 public class StreamsImplementation {
-    //Test
-    int a = 100;
+
+
+    public static  void main(String[] args) {
+//Finding the first letter 'S' in the List of String Using Java 8  Lamda features
+        List<String> list = new LinkedList<>();
+        list.add("Premananth");
+        list.add("Mute");
+        list.add("Mona");
+        list.add("Srivathsan");
+        list.add("Sriram");
+
+       Optional<String> s = list.stream()
+               .filter(x->x.startsWith("S")).findFirst();
+        System.out.printf("String Found is :::" + s);
+    }
 }
